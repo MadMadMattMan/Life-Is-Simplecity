9c965867a75f1a738b1e55dfe28ec1260b1d8b19
GetAnneFlanked <blake.richardson369@gmail.com>
2026-08-15 00:44:31 +1200

    AI navigation start

---
+++ Assets/NPCManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class NPCManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> OrderQueue = new List<GameObject>();
+    [SerializeField] private List<GameObject> WaitingQueue = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
